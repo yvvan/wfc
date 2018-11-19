bd5f037e2d74dbd6c8ceffd15bbe5c3c0d48234a
Sebastian Schweigert <sebastian341@gmail.com>
2018-11-18 22:35:26 -0800

    Formatting and refactoring arrays.hpp

--- arrays.hpp
+++ arrays.hpp
@@ -2,60 +2,81 @@
 
 #include <vector>
 
-template<typename T>
-struct Array2D
+struct Dimension2D
 {
-public:
-	Array2D() : _width(0), _height(0) {}
-	Array2D(size_t w, size_t h, T value = {})
-		: _width(w), _height(h), _data(w * h, value) {}
 
-	const size_t index(size_t x, size_t y) const
-	{
-		DCHECK_LT_F(x, _width);
-		DCHECK_LT_F(y, _height);
-		return y * _width + x;
-	}
+    size_t width;
 
-	inline       T& mut_ref(size_t x, size_t y)       { return _data[index(x, y)]; }
-	inline const T&     ref(size_t x, size_t y) const { return _data[index(x, y)]; }
-	inline       T      get(size_t x, size_t y) const { return _data[index(x, y)]; }
-	inline void set(size_t x, size_t y, const T& value) { _data[index(x, y)] = value; }
+    size_t height;
 
-	size_t   width()  const { return _width;       }
-	size_t   height() const { return _height;      }
-	const T* data()   const { return _data.data(); }
-
-private:
-	size_t _width, _height;
-	std::vector<T> _data;
 };
 
-template<typename T>
-struct Array3D
+template <class T>
+class Array2D
 {
-public:
-	Array3D() : _width(0), _height(0), _depth(0) {}
-	Array3D(size_t w, size_t h, size_t d, T value = {})
-		: _width(w), _height(h), _depth(d), _data(w * h * d, value) {}
 
-	const size_t index(size_t x, size_t y, size_t z) const
-	{
-		DCHECK_LT_F(x, _width);
-		DCHECK_LT_F(y, _height);
-		DCHECK_LT_F(z, _depth);
-		// return z * _width * _height + y * _width + x;
-		return x * _height * _depth + y * _depth + z; // better cache hit ratio in our use case
-	}
+	public:
 
-	inline       T& mut_ref(size_t x, size_t y, size_t z)       { return _data[index(x, y, z)]; }
-	inline const T&     ref(size_t x, size_t y, size_t z) const { return _data[index(x, y, z)]; }
-	inline       T      get(size_t x, size_t y, size_t z) const { return _data[index(x, y, z)]; }
-	inline void set(size_t x, size_t y, size_t z, const T& value) { _data[index(x, y, z)] = value; }
+		Array2D() : 
+            mDimensions{0, 0}
+        {}
 
-	inline size_t size() const { return _data.size(); }
+		Array2D(size_t w, size_t h, T value = {}) : 
+            mDimensions{w, h}, 
+            mData(w * h, value)
+        {}
 
-private:
-	size_t _width, _height, _depth;
-	std::vector<T> _data;
+		inline       T& mut_ref(size_t x, size_t y)       { return mData[index(x, y)]; }
+		inline const T&     ref(size_t x, size_t y) const { return mData[index(x, y)]; }
+		inline       T      get(size_t x, size_t y) const { return mData[index(x, y)]; }
+		inline void set(size_t x, size_t y, const T& value) { mData[index(x, y)] = value; }
+
+		size_t   width()  const { return mDimensions.width;       }
+		size_t   height() const { return mDimensions.height;      }
+		const T* data()   const { return mData.data(); }
+	private:
+
+		size_t index(size_t x, size_t y) const
+		{
+			return y * mDimensions.width + x;
+		}
+
+        Dimension2D mDimensions;
+
+		std::vector<T> mData;
+
+};
+
+template <class T>
+class Array3D
+{
+
+	public: 
+
+		Array3D() : mWidth(0), mHeight(0), mDepth(0) {}
+		Array3D(size_t w, size_t h, size_t d, T value = {})
+			: mWidth(w), mHeight(h), mDepth(d), mData(w * h * d, value) {}
+
+		const size_t index(size_t x, size_t y, size_t z) const
+		{
+			DCHECK_LT_F(x, mWidth);
+			DCHECK_LT_F(y, mHeight);
+			DCHECK_LT_F(z, mDepth);
+			// return z * mWidth * mHeight + y * mWidth + x;
+			return x * mHeight * mDepth + y * mDepth + z; // better cache hit ratio in our use case
+		}
+
+		inline       T& mut_ref(size_t x, size_t y, size_t z)       { return mData[index(x, y, z)]; }
+		inline const T&     ref(size_t x, size_t y, size_t z) const { return mData[index(x, y, z)]; }
+		inline       T      get(size_t x, size_t y, size_t z) const { return mData[index(x, y, z)]; }
+		inline void set(size_t x, size_t y, size_t z, const T& value) { mData[index(x, y, z)] = value; }
+
+		inline size_t size() const { return mData.size(); }
+
+	private:
+
+		size_t mWidth, mHeight, mDepth;
+
+		std::vector<T> mData;
+
 };
