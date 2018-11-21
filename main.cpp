a73a75973f1127973c6e14302842857f04b3ac20
Sebastian Schweigert <sebastian341@gmail.com>
2018-11-20 21:31:14 -0800

    Remove Options

--- main.cpp
+++ main.cpp
@@ -947,7 +947,7 @@ Result run(Output* output, const Model& model, size_t seed, size_t limit, jo_gif
 	return Result::kUnfinished;
 }
 
-void run_and_write(const Options& options, const std::string& name, const configuru::Config& config, const Model& model)
+void run_and_write(const std::string& name, const configuru::Config& config, const Model& model)
 {
 	const size_t limit       = config.get_or("limit",       0);
 	const size_t screenshots = config.get_or("screenshots", 2);
@@ -958,8 +958,6 @@ void run_and_write(const Options& options, const std::string& name, const config
 			int seed = rand();
 
 			Output output = create_output(model);
-
-			jo_gif_t gif;
 
 			const auto result = run(&output, model, seed, limit, nullptr);
 
@@ -1021,7 +1019,7 @@ std::unique_ptr<Model> make_tiled(const std::string& image_dir, const configuru:
 	return std::make_unique<TileModel>(tile_config, subset, out_width, out_height, periodic, tile_loader);
 }
 
-void run_config_file(const Options& options, const std::string& path)
+void run_config_file(const std::string& path)
 {
 	LOG_F(INFO, "Running all samples in %s", path.c_str());
 	const auto samples = configuru::parse_file(path, configuru::CFG);
@@ -1031,7 +1029,7 @@ void run_config_file(const Options& options, const std::string& path)
 		for (const auto& p : samples["overlapping"].as_object()) {
 			LOG_SCOPE_F(INFO, "%s", p.key().c_str());
 			const auto model = make_overlapping(image_dir, p.value());
-			run_and_write(options, p.key(), p.value(), *model);
+			run_and_write(p.key(), p.value(), *model);
 			p.value().check_dangling();
 		}
 	}
@@ -1040,7 +1038,7 @@ void run_config_file(const Options& options, const std::string& path)
 		for (const auto& p : samples["tiled"].as_object()) {
 			LOG_SCOPE_F(INFO, "Tiled %s", p.key().c_str());
 			const auto model = make_tiled(image_dir, p.value());
-			run_and_write(options, p.key(), p.value(), *model);
+			run_and_write(p.key(), p.value(), *model);
 		}
 	}
 }
@@ -1048,8 +1046,6 @@ void run_config_file(const Options& options, const std::string& path)
 int main(int argc, char* argv[])
 {
 	loguru::init(argc, argv);
-
-	Options options;
 
 	std::vector<std::string> files;
 
@@ -1067,6 +1063,6 @@ int main(int argc, char* argv[])
 	}
 
 	for (const auto& file : files) {
-		run_config_file(options, file);
+		run_config_file(file);
 	}
 }
