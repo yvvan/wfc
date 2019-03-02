09130545c00e3256e572e7e5c5e6869a1ef8b04e
Sebastian Schweigert <sebastian341@gmail.com>
2019-03-02 15:32:26 -0800

    WIP

--- test/src/overlapping_pattern_extraction_test.cpp
+++ test/src/overlapping_pattern_extraction_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <iostream>
+#include <algorithm>
 
 #include <wfc/ranges.h>
 
@@ -10,6 +11,8 @@
 constexpr RGBA white = { 255, 255, 255 };
 constexpr RGBA black = { 0, 0, 0 };
 
+// Makes a black/white checkerboard pattern of the desired size where the bottom left
+// corner
 PalettedImage checkerBoard(size_t width)
 {
 	Dimension2D dimension{ width, width }; 
@@ -32,12 +35,54 @@ PalettedImage checkerBoard(size_t width)
 	};
 }
 
+bool patternsEquivalent(const PatternOccurrence& left, const PatternOccurrence& right)
+{
+	return true;
+}
+
+bool imagePropertiesEquivalent(const ImagePatternProperties& left, const ImagePatternProperties& right)
+{
+	// Pattern
+	if (left.patterns.size() != right.patterns.size())
+	{
+		return false;
+	}
+
+	bool patternsMatch = std::is_permutation(left.patterns.begin(), left.patterns.end(), right.patterns.begin(), patternsEquivalent);
+	if (!patternsMatch)
+	{
+		return false;
+	}
+
+	// Map
+	return true;
+}
+
 TEST(OverlappingExtractionTest, test1)
 {
 	const size_t size = 4;
-	const int n = 4;
+	const int n = 2;
 
 	auto sample = checkerBoard(size);
+
+	Pattern pattern({ n, n });
+	pattern[{0, 0 }] = 0;
+	pattern[{0, 1 }] = 1;
+	pattern[{1, 0 }] = 1;
+	pattern[{1, 1 }] = 0;
+	Array2D<PatternIdentifier> grid({ size, size });;
+
+	ImagePatternProperties expectedProperties = 
+	{
+		.patterns = 
+		{
+			{
+				.pattern = pattern,
+				.occurrence = {{ size * size }}
+			}
+		},
+		.grid = grid
+	};
 
 	ImagePatternProperties properties = extractPatternsFromImage(sample, n);
 
